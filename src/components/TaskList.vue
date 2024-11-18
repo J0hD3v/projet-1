3089f408d15ca826794e1de2e994f2ae826cd51e
J0hD3v <johanfievet@orange.fr>
2024-11-18 11:27:57 +0100

    creation du projet

---
+++ src/components/TaskList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <input v-model="newTask" />
+    <button v-on:click="addTask(newTask)">Ajouter</button>
+    <li v-for="task in tasks">
+      {{ task }}
+    </li>
+  </div>
+</template>
+
+<script lang='js'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TaskList',
+  /* components: {
+  },
+  props: {
+    // v-model
+    modelValue: {
+      default: '',
+    },
+  },
+  emits: {
+    // v-model event with validation
+    'update:modelValue': (value) => value !== null,
+  }, */
+  data() {
+    return {
+      newTask: "",
+      tasks: [
+        "ceci est la tache 1",
+        "ceci est la tache 2",
+        "ceci est la tache 3",
+      ]
+    };
+  },
+  methods: {
+    addTask(newTask) {
+      this.tasks.push(newTask);
+    }
+  },
+  /* computed: {
+    value: {
+      get () {
+        return this.modelValue;
+      },
+      set (value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
+  },
+  watch: {
+    modelValue: {
+      async handler (_newValue, _oldValue) {
+        // do something
+      },
+      immediate: true
+    },
+  },
+  beforeMount() {
+  },
+  mounted() {
+  },
+  updated() {
+  },
+  beforeUnmount() {
+    // stop the wacher on modelValue
+    this.$watch('modelValue', () => {}, {});
+  }, */
+});
+</script>
+
+<style scoped lang="css">
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TaskList from '../components/TaskList.vue';
+</script>
+
+<template>
+  <div class="d-flex flex-column align-items-center min-vh-100 mt-5">
+    <h1 class="m-4">Home View</h1>
+    <TaskList />
+  </div>
+</template>
