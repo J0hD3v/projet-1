750a1b278471c80f4abe50fc1c0683938ffe893c
J0hD3v <johanfievet@orange.fr>
2024-11-18 13:44:30 +0100

    patch

--- src/components/exercice-condition.vue
+++ src/components/exercice-condition.vue
@@ -9,50 +9,10 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'ExerciceCondition',
-  /* components: {
-  },
-  props: {
-    // v-model
-    modelValue: {
-      default: '',
-    },
-  },
-  emits: {
-    // v-model event with validation
-    'update:modelValue': (value) => value !== null,
-  }, */
   data() {
     return {
     };
   },
-  /* computed: {
-    value: {
-      get () {
-        return this.modelValue;
-      },
-      set (value) {
-        this.$emit('update:modelValue', value);
-      },
-    },
-  },
-  watch: {
-    modelValue: {
-      async handler (_newValue, _oldValue) {
-        // do something
-      },
-      immediate: true
-    },
-  },
-  beforeMount() {
-  },
-  mounted() {
-  },
-  updated() {
-  },
-  beforeUnmount() {
-    // stop the wacher on modelValue
-    this.$watch('modelValue', () => {}, {});
-  }, */
   methods: {
     affichageConditionnel() {
       return Math.random() > 0.5 ? "Jambon" : "Fromage";
